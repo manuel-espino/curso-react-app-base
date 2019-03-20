7e58cad2e7153d356922901338250aff036bc7fd
Manuel Espino <manudrago@gmail.com>
2019-03-20 11:29:23 +0100

    Started to modify the SearchContainer

--- src/containers/SearchContainer/SearchContainer.js
+++ src/containers/SearchContainer/SearchContainer.js
@@ -1,12 +1,23 @@
 import React, { PropTypes } from 'react';
 
 // Importamos los componentes
-
+import Header from '../../components/Header';
+import SearchForm from '../../components/Header';
 
 /**
  * Muestra un buscador, así como la lista de resultados.
  */
 class SearchContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      loading: false, //Flag
+      results: [],
+      search: '',
+      queried: false //Para saber si se ha producido una búsqueda y cambiar el mensaje
+    }
+  }
   /**
    * Datos falsos. Los utilizamos en desarrollo hasta que leamos los datos de
    * la API.
@@ -36,11 +47,17 @@ class SearchContainer extends React.Component {
     ]
   }
 
+  onSubmit(value){
+    
+  }
+
   /**
    * Render the SearchContainer component
    */
   render() {
     return <main className="container">
+    <Header />
+    <SearchForm onSubmit={ this.onSubmit } search={ this.state.search}/>
       <h1>Búsqueda</h1>
     </main>
   }
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Importamos los componentes
-import Header from './components/Header';
-// import SearchContainer from './containers/SearchContainer';
+//import Header from './components/Header';
+ import SearchContainer from './containers/SearchContainer';
 // import DetailsContainer from './containers/DetailsContainer';
 
 ReactDOM.render(
-  <Header />,
-  // <SearchContainer />,
+  // <Header />,
+   <SearchContainer />,
   // <DetailsContainer />,
   document.getElementById('root')
 );
