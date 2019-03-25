ad64d9dea0f3a532653e155a394887fce8ff7430
Manuel Espino <manuel@secmotic.com>
2019-03-25 15:01:30 +0100

    Redux incorporated

---
+++ src/reducers/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+
+    loading: false, //Flag
+    results: [],
+    search: '',
+    queried: false //Para saber si se ha producido una búsqueda y cambiar el mensaje
+  }
+
+  const reducer = (state = initialState, action) => {
+switch(action){
+    case 'SEARCH_START':{
+
+    }
+    case 'SEARCH_SUCCESS': {
+
+    }
+}
+  }
